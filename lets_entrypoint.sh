d943d5ed429c3b22dcc0191267998971554d7c79
Lucas Zanela <me@lucaszanella.com>
2018-04-11 16:25:06 -0300

    Update lets_entrypoint.sh

--- lets_entrypoint.sh
+++ lets_entrypoint.sh
@@ -1,6 +1,6 @@
 if [ ! -f /etc/letsencrypt/live/lucaszanella.com/cert.pem ] then 
 
-certbot certonly --webroot --webroot-path /var/www/html --non-interactive --agree-tos --email me@lucaszanella.com --cert-name lucaszanella.com --domains lucaszanella.com; \
+certbot certonly --webroot --webroot-path /var/www/html --non-interactive --agree-tos --email me@lucaszanella.com --cert-name lucaszanella.com --domains lucaszanella.com \
 && if [ -f /etc/letsencrypt/live/lucaszanella.com/cert.pem ] then  
         echo '<VirtualHost _default_:443>\n\
         ServerName lucaszanella.com:443\n\
